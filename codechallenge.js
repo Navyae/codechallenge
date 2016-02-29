8c282d69a3336597b02585e5ab4fa86392c86fc1
Navya Eetaram <navyaeetaram17@gmail.com>
2016-02-29 08:37:05 -0600

    more comments

--- codechallenge.js
+++ codechallenge.js
@@ -2,6 +2,7 @@
 
   if (Meteor.isClient) {
 
+          // code to run on server at startup
   Template.hello.rendered = function() {
 
      $( "#sprice" ).slider({
@@ -17,36 +18,37 @@
           var por=$(this).find(".ui-slider-handle:last").position().left;
           var po = (pol+por)/2;
          $( "#average" ).val( "$" + ave +" "+"Average" );
-         $('.s-pr-caret').css({"position": "absolute","left": po,'display':'block'});
+         $('.module-pr-caret').css({"position": "absolute","left": po,'display':'block'});
 
        }
      });
-    $('.s-pr-caret').css({'display':'none'});
+    $('.module-pr-caret').css({'display':'none'});
 
 
    };
   var unc="null";
   Template.hello.events({
 
-    'click .s-pick-check-button': function(e) {
+    'click .module-pick-check-button': function(e) {
       if(unc == (e.currentTarget)){
-         $(e.target).children('.s-pick-check-button-chec').find('i').css({'color':'#fff'});
+         $(e.target).children('.module-pick-check-button-chec').find('i').css({'color':'#fff'});
          unc ='';
        }
        else{
          unc = (e.currentTarget);
-         $(e.target).children('.s-pick-check-button-chec').find('i').css({'color':'#44b244'});
+         $(e.target).children('.module-pick-check-button-chec').find('i').css({'color':'#44b244'});
 
        }
       },
 
   });
 
+
+
   }
 
   if (Meteor.isServer) {
     Meteor.startup(function () {
 
-      // code to run on server at startup
     });
   }
